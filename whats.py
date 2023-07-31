82b28ef8a69ef7e29e82081efa1504947cd8a991
rajpratham13 <138620787+rajpratham13@users.noreply.github.com>
2023-07-31 23:31:44 +0530

    Add files via upload

---
+++ whats.py
@@ -0,0 +1,18 @@
+import pyautogui as pg
+import time
+count =1
+while count<=1000:
+    pg.click(112,764) 
+    time.sleep(0.1)
+    pg.write('call karo na aalu ')
+    time.sleep(0.1)
+    pg.press('enter')
+    time.sleep(0.1)
+    count+=1
+    
+
+
+    
+
+   
+    
